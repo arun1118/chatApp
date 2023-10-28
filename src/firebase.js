31682747db032c6a1ef21d9682f5a4be4fd90ccc
arun1118 <arunpanigrahi18@gmail.com>
2023-10-28 20:27:09 +0530

    add firebase, register a user and upload profile image

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB1K6FbzaTlJ8gB7rov3d_BGyg6mFSNC-c",
+  authDomain: "chat-d5b6d.firebaseapp.com",
+  projectId: "chat-d5b6d",
+  storageBucket: "chat-d5b6d.appspot.com",
+  messagingSenderId: "727342513360",
+  appId: "1:727342513360:web:c2f717538f9f2df498b2d5"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const storage = getStorage();
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AddAvatar from "../img/addAvatar.png";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth,storage } from '../firebase';
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 const Register = () => {
+
+  const [err,setErr]=useState(false);
+  
+  const handleSubmit=async (e)=>{
+    e.preventDefault();
+    const displayName=e.target[0].value;
+    const email=e.target[1].value;
+    const password=e.target[2].value;
+    const file=e.target[3].files[0];
+
+    try {
+      const res=await createUserWithEmailAndPassword(auth, email, password);
+      console.log(res);
+      const storageRef = ref(storage, displayName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      
+      uploadTask.on('state_changed'
+      ,(snapshot)=>{
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch(snapshot.state){
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+        }
+      }
+      ,(error)=>{
+        setErr(true);
+      }
+      ,()=>{
+        getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL)=>{
+        console.log('File available at', downloadURL);
+        await updateProfile(res.user, {displayName, photoURL: downloadURL})
+      });
+      }
+      );
+    } 
+    catch (error) {
+      setErr(true);
+    }
+  }
+
   return (
     <div className='formContainer'>
         <div className="formWrapper">
             <span className="logo">Talkative</span>
             <span className="title">Login</span>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="text" placeholder='Enter your name'/>
                 <input type="email" placeholder='Enter your email'/>
                 <input type="password" placeholder='Enter your password'/>
@@ -17,6 +65,7 @@ const Register = () => {
                     <span>Add and Avatar</span>
                 </label>
                 <button>Register</button>
+                {err && <sapn>Something went wrong!</sapn>}
             </form>
             <p>Already have an account? Login</p>
         </div>
